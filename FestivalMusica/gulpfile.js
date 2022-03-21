2edd1d14f31a7c69b280f5c794580f3ad996f285
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-03-21 13:04:11 -0300

    finalizado

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -2,19 +2,27 @@ const { src, dest, watch, parallel } = require('gulp');
 //css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //Images
 const cache = require('gulp-cache');
 const imageMin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
+//Javascript
+const terser = require('gulp-terser-js');
 
-function css(done){
-    
+function css(done){    
     src('src/scss/**/*.scss')
-    .pipe( plumber())
-    .pipe(sass())
-    .pipe(dest('build/css'))
+        .pipe(sourcemaps.init())
+        .pipe( plumber())
+        .pipe(sass())
+        .pipe( postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'))
     done();
 }
 
@@ -40,6 +48,9 @@ function versionWebp(done){
 
 function javascript(done){
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     done();
 }
--- FestivalMusica/src/js/app.js
+++ FestivalMusica/src/js/app.js
@@ -11,13 +11,15 @@ function iniciarApp(){
 function navegacionFija(){
     const barra=document.querySelector('.header')
     const sobreFestival=document.querySelector('.sobre-festival');
+    const body=document.querySelector('body');
 
     window.addEventListener('scroll', function(){
-
-        if(sobreFestival.getBoundingClientRect().top <0){
+        if(sobreFestival.getBoundingClientRect().bottom < 200){
             barra.classList.add('fijo');
+            body.classList.add('body-scroll');
         }else{
             barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
         }
     });
 };
